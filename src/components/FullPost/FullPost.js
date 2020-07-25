7e1c934fcc3771a70465c7711054c47a1c29615f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-25 22:06:01 +0530

    Added delete support

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -23,6 +23,13 @@ class FullPost extends Component {
     }
   }
 
+  deleteButtonHandler = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+    .then(response => {
+      console.log(response);
+    })
+  }
+
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
     if (this.props.id) post = <p style={{ textAlign: 'center' }}>Loading...</p>;
@@ -32,7 +39,7 @@ class FullPost extends Component {
           <h1> {this.state.loadedPost.title} </h1>
           <p> {this.state.loadedPost.body} </p>
           <div className='Edit'>
-            <button className='Delete'>Delete</button>
+            <button onClick={this.deleteButtonHandler} className='Delete'>Delete</button>
           </div>
         </div>
       );
